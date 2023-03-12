4239211233f14955cc723788eb9cf813142a6714
최근원 <kidjustinchoi@gmail.com>
2023-03-12 20:59:51 +0900

    Initialize project using Create React App, and make accordion with framer motion

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { GlobalStyle } from "./style/GlobalStyle";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
